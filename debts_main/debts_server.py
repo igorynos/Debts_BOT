8d41efb78d49dc0e7456a1169f982d88167ab596
igorynos <igorynos@gmail.com>
2024-02-05 03:00:34 +0400

    merge the end

--- debts_main/debts_server.py
+++ debts_main/debts_server.py
@@ -651,15 +651,15 @@ class DebtsServer(object):
                     raise ValueError('Пользователя нет в базе данных')
         else:
             with self.connection.cursor() as cursor:
-            if isinstance(acc_id, int):
-                    query = "SELECT id FROM groups WHERE accounting_id = %s AND user_id = %s"
-                    cursor.execute(query, (acc_id, user))
+                if isinstance(acc_id, int):
+                        query = "SELECT id FROM groups WHERE accounting_id = %s AND user_id = %s"
+                        cursor.execute(query, (acc_id, user))
                 else:
-                query = "SELECT id FROM users WHERE id = %s"
-                cursor.execute(query, user)
-            users = cursor.fetchall()
-                if len(users) == 0:
-                    raise ValueError('Пользователь не входит в групу рассчета')
+                    query = "SELECT id FROM users WHERE id = %s"
+                    cursor.execute(query, user)
+                users = cursor.fetchall()
+                    if len(users) == 0:
+                        raise ValueError('Пользователь не входит в групу рассчета')
 
     # noinspection PyTypeChecker
     @try_and_log("Ошибка создания документа 'Покупка'")
