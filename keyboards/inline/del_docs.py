f06931e8833ad1efb0d7416abd41885d4f9c1d9a
YaroslavSapozhnikov <yars@narod.ru>
2024-03-08 12:19:43 +0300

    "Покуки"

--- keyboards/inline/del_docs.py
+++ keyboards/inline/del_docs.py
@@ -2,7 +2,7 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from keyboards.inline.callback_data import del_docs_callback
 
 delete_docs = InlineKeyboardMarkup().add(InlineKeyboardButton(
-    "Покуки", callback_data='del_docs_purch'), InlineKeyboardButton(
+    "Покупки", callback_data='del_docs_purch'), InlineKeyboardButton(
         "Платежи", callback_data='del_docs_pay'))
 
 accept_delete_docs = InlineKeyboardMarkup().add(InlineKeyboardButton(
